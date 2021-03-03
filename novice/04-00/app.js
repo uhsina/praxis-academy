f208172057f27f8a302990451081d7ae63a66ed2
Uhsina Huda <uhsinahuda@gmail.com>
2021-03-03 10:56:57 +0700

    mvc sederhana

--- novice/04-00/app.js
+++ novice/04-00/app.js
@@ -5,7 +5,9 @@ const express = require('express');
 //use hbs view engine
 const hbs = require('hbs');
 const app = express();
- 
+const { Client } = require('pg')
+// for connection information 
+
 //set views file
 app.set('views',path.join(__dirname,'views'));
 //set view engine
@@ -14,7 +16,33 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 
-//route untuk halaman home
+const db = new Client({
+  database: 'akun',
+  user: 'postgres',
+  password:'123123123'
+})
+
+db.connect(function(err) {
+  if (err) throw err;
+  let pg = `create table iduser(
+    id varchar(3),
+    nama char(20)
+  ) `
+  db.query(pg, function(err, result) {
+    console.log("Tabel sudah dibuat!")
+  });
+});
+//===========================
+
+app.get('/user',(req,res)=>{
+  let pg='select *from iduser';
+  let query =db.query(pg,(err,result)=>{
+    if (err) throw err;
+    console.log(result)
+    res.render('main',{employee:result.rows})
+  })
+})
+
 /*
 app.get('/',(req, res) => {
   //render file index.hbs
@@ -37,7 +65,7 @@ app.get('/:name',(req, res) => {
 //app.get('/', (req, res)=>{ 
 //    res.render('demo') 
 //}) 
-
+/*
 var demo = { 
 	nama : 'Lohan',
 	umur : 26,
@@ -47,10 +75,10 @@ var demo = {
 } 
 
 app.get('/', (req, res)=>{ 
-	res.render('dynamic', {demo : demo}) 
-}) 
+	res.render('dynamic', {demo:demo}) 
+}) */
 
-
+/*
 var projects = { 
 	nama : 'Regul', 
 	skills : ['node js', 'front end', 'python'] 
@@ -59,11 +87,13 @@ var projects = {
 app.get('/projects', (req, res)=>{ 
 	res.render('projects', {projects : projects});
 }) 
+*/
 
 
 
-app.listen(8000, () => {
-  console.log('Server is running at port 8000');
+
+app.listen(3000, () => {
+  console.log('Server is running at port 3k');
 });
 
 
